52b7535aee4627d771ffc89e65e53d811f46075f
Alain Armand <aarmand.inbox@users.noreply.gmail.com>
2019-11-12 10:35:10 -0500

    Updates

---
+++ precache-manifest.115f0662d8f91da6fa9bab489c4097ee.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9ea99a9e5cc4b4f95f859c16d22db4d",
+    "url": "/brandt-plus-krutikova-simple/index.html"
+  },
+  {
+    "revision": "d4b39caca651a4159345",
+    "url": "/brandt-plus-krutikova-simple/static/css/main.c41158f1.chunk.css"
+  },
+  {
+    "revision": "56d22811780033a26555",
+    "url": "/brandt-plus-krutikova-simple/static/js/2.54137f9e.chunk.js"
+  },
+  {
+    "revision": "d4b39caca651a4159345",
+    "url": "/brandt-plus-krutikova-simple/static/js/main.4d421af3.chunk.js"
+  },
+  {
+    "revision": "149e80e06d1e59150a7f",
+    "url": "/brandt-plus-krutikova-simple/static/js/runtime~main.2df0c11a.js"
+  },
+  {
+    "revision": "e175412e95b9aeba27a71b47937e0fad",
+    "url": "/brandt-plus-krutikova-simple/static/media/bensound-creativeminds.e175412e.mp3"
+  },
+  {
+    "revision": "fd79b1142f66f1981d0aea1a0b1501ea",
+    "url": "/brandt-plus-krutikova-simple/static/media/bensound-summer.fd79b114.mp3"
+  },
+  {
+    "revision": "9569869286a4e88efa8724d24f50bf47",
+    "url": "/brandt-plus-krutikova-simple/static/media/bensound-ukulele.95698692.mp3"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/brandt-plus-krutikova-simple/precache-manifest.115f0662d8f91da6fa9bab489c4097ee.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/brandt-plus-krutikova-simple/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
